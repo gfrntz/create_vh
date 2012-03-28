175fc2d3bf7cc9b0e789873fd6684f28a5d9e9ba
Cepnoy <chainwolf@gmail.com>
2012-03-28 21:55:03 +0400

    Modify

--- do-vh.pl
+++ do-vh.pl
@@ -6,11 +6,28 @@ use strict;
 use vh;   # only for apache/http
 #use isp; # for isp manager
 
-my ($user_name, $vh_name);
+my ($user_name, $vh_name, $whoami);
 
+if ( chomp($whoami = `whoami`) ne "root" ) {
+	print "Only root can run me.\n";
+	exit;
+}
+
+if (@ARGV > 2) {
+	print "To many args. Exit.\n";
+	exit;
+	
+} elsif (@ARGV == 0 || @ARGV == 1) {
+	print "I need two args.\n";
+	exit;
+}
+
+$user_name = $ARGV[0];
+$vh_name = $ARGV[1];
 
 &chk_dir;
 
+print "User name = $user_name ; vh name = $vh_name\n";
 
 create_vh($user_name,$vh_name);
 
--- vh.pm
+++ vh.pm
@@ -27,7 +27,7 @@ sub create_vh {
 	$user_name = shift;
 	$vh_name = shift;
 
-	open (VH, ">", "$apa_dir" . "$vh_name" . ".conf");
+	open (VH, ">", "$apa_dir" . "/$vh_name" . ".conf");
 	print VH <<EOF;
 
 #
@@ -36,7 +36,7 @@ sub create_vh {
 
 <VirtualHost *:80>
     ServerAdmin postmaster\@$vh_name
-    DocumentRoot /var/www/$username/$vh_name
+    DocumentRoot /var/www/$user_name/$vh_name
     DirectoryIndex index.php index.html index.htm
     ErrorLog /var/www/$user_name/apache-log/$vh_name.error.log
     CustomLog /var/www/$user_name/apache-log/$vh_name.access.log common
@@ -46,7 +46,24 @@ sub create_vh {
 EOF
 
 	close(VH);
+	
+	if ( -d "/var/www/$user_name/$vh_name" ) {
+		print "Virtual host directory for $vh_name exists.\n";
+		print "Skip create directory.\n";
+	} else {
+		print "Create dir for $vh_name\n";
+		`mkdir /var/www/$user_name/$vh_name`;
+	}
 
+	if ( -d "/var/www/$user_name/apache-log" ) {
+		print "Apache log dir in user folder exists. Skip.\n";
+	} else {
+		print "Create apache log directory.\n";
+		`mkdir /var/www/$user_name/apache-log`;
+	}
+	
+	`touch /var/www/$user_name/apache-log/$vh_name.error.log`;
+	`touch /var/www/$user_name/apache-log/$vh_name.access.log`;
 }
 
 return1;
